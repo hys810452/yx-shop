3c50032712e7baba3d1c69881665aecd9d3fa987
hys810452 <hys810452@163.com>
2019-08-02 22:19:47 +0800

    第一次提交

---
+++ web/src/components/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="index">
+    <van-row type="flex" justify="center">
+      <van-col span="3" align="center" class="text-c">
+        <h2>首页</h2>
+      </van-col>
+      <van-col span="18">
+        <van-search shape="round" placeholder="请输入搜索关键词" v-model="search" />
+      </van-col>
+      <van-col span="3" align="center">消息</van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    war() {}
+  }
+};
+</script>
+
+<style scoped>
+.text-c {
+  line-height: 3.5rem;
+}
+</style>
---
+++ web/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vant from 'vant'
+// vant样式
+import 'vant/lib/index.css'
+// 全局css样式
+import './assets/css/main.css'
+
+Vue.use(Vant);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>'
+})
